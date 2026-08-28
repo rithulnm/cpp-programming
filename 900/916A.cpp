aee6d29d258e0ba9e24aaa244fb9885edc30ed2e
Rithul <25je0402@iitism.ac,in>
2026-08-29 01:17:31 +0530

    Day4

--- 900/916A.cpp
+++ 900/916A.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
 
-bool hasSeven(int n){
-    while (n > 0){
-        if (n%10==7){
-            return true;
-        }
-        n /= 10;
+int main() {
+  int x;
+  std::cin >> x;
+  int h, m;
+
+  std::cin >> h >> m;
+  int ans = 0;
+
+  while (h % 10 != 7 && m % 10 != 7) {
+    if (m < x) {
+      m = (m + 60 - x) % 60;
+      h = (h == 0) ? 23 : h - 1;
+    } else {
+      m -= x;
     }
-    return false;
-}
-
-int main(){
-    int x,hh,mm;
-    std::cin >> x;
-    std::cin >> hh >> mm;
-    int h = hh;
-    int m = mm;
-    int ti = h*60 + m;
-
-    while(true){
-        if (mm <= 0){
-            mm += 60;
-            hh --;
-        }
-        if (hasSeven(hh) || hasSeven(mm)){
-            break;
-        }
-        mm = mm - x;
-    }
-    int tf = (hh*60) + mm;
-    std::cout << (tf-ti)/(60*x) << '\n';
-
+    ans++;
+  }
+  std::cout << ans << '\n';
 }
